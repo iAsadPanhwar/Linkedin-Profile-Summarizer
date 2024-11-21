fa35c9ce9299d8b741110b8fcd88fec30d068263
Asad Ali Panhwar <asadalipuh5@gmail.com>
2024-11-22 03:21:15 +0500

    main.py updated

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
-from dotenv import load_dotenv
 import os
+from langchain import hub
+from langchain.agents import create_react_agent, AgentExecutor
+from langchain_groq import ChatGroq
+from langchain_core.prompts import PromptTemplate
+from dotenv import load_dotenv
+from tools.linkedin_url import linkedin_url
+from tools.webscrap import webscrap
+
+load_dotenv()
 
